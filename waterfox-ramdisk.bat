aa7d92b596cdd1a1e57ac831b427d2fc02dbb00f
jack4455667788 <jack44556677@yahoo.com>
2019-02-14 23:47:20 -0500

    Update waterfox-ramdisk.bat

--- waterfox-ramdisk.bat
+++ waterfox-ramdisk.bat
@@ -1,40 +1,139 @@
-echo off
+@echo off
 :: browser.cache.disk.enable;false
 :: browser.cache.memory.max_entry_size;51200 - To match default browser.cache.disk.max_entry_size;51200
 :: cd %USERPROFILE%\AppData\Roaming\Waterfox
+:: http://istomin.de/2015/02/23/windows-disable-taskbar-jump-lists/
 :: TODO Read from profile.ini to extract the correct active profile path
 
 :: mkdir p68xw6kw.default.backup
 :: Move command below fails (access denied) for unknown reasons, potentially permissions related?  User has no issue moving folder.
-::move p68xw6kw.default W:\
+:: move p68xw6kw.default W:\
+setlocal
 
-cd %USERPROFILE%\AppData\Roaming\Waterfox\Profiles
+:: set defaultprofilefoldername="p68xw6kw.default"
+
 C:
-FOR /F "tokens=*" %%g IN ('dir /AL /B') DO (set crashtest=%%g)
+cd %USERPROFILE%\AppData\Local\Waterfox\Profiles
 
-IF NOT EXIST W:\ (
+FOR /F "tokens=*" %%g IN ('dir /AL /B 2^> nul') DO (set crashtest1=%%g)
+
+C:
+cd %USERPROFILE%\AppData\Roaming\Waterfox\Profiles
+
+FOR /F "tokens=*" %%g IN ('dir /AL /B 2^> nul') DO (set crashtest=%%g)
+
+IF NOT EXIST W:\ 2> nul (
 	IF "%crashtest%" == "p68xw6kw.default" (
 		echo "Computer crashed, or was otherwise restarted without copying the ramdisk profile back to the SSD.  Restoring from last backup, some browsing history was likely lost. Press any key to continue."
 		pause
+		C:
 		cd %USERPROFILE%\AppData\Roaming\Waterfox\Profiles
 		rmdir p68xw6kw.default
+		IF NOT EXIST %USERPROFILE%\AppData\Roaming\Waterfox\Profiles\p68xw6kw.default.backup (
+			echo "Roaming Backup not found as expected, aborting!"
+			pause
+			exit
+		)
 		ren p68xw6kw.default.backup p68xw6kw.default
+		ren p68xw6kw.default.backup.previous p68xw6kw.default.backup
+		ren p68xw6kw.default.backup.previous.previous p68xw6kw.default.backup.previous
+		:: echo "done deleting roaming symlink"
+		:: pause
 		
-		cd %USERPROFILE%\AppData\Local\Waterfox\Profiles
 		C:
-		FOR /F "tokens=*" %%g IN ('dir /AL /B') DO (set crashtest=%%g)
-	
-		IF "%crashtest%" == "p68xw6kw.default" (
+		cd %USERPROFILE%\AppData\Local\Waterfox\Profiles
+		IF "%crashtest1%" == "p68xw6kw.default" (
+			C:
 			cd %USERPROFILE%\AppData\Local\Waterfox\Profiles
 			rmdir p68xw6kw.default
+			IF NOT EXIST %USERPROFILE%\AppData\Local\Waterfox\Profiles\p68xw6kw.default.backup (
+				echo "Local Backup not found as expected, aborting!"
+				pause
+				exit
+			)
+			ren p68xw6kw.default.backup p68xw6kw.default
+			ren p68xw6kw.default.backup.previous p68xw6kw.default.backup
+			ren p68xw6kw.default.backup.previous.previous p68xw6kw.default.backup.previous
+		)
+	) ELSE (
+		IF NOT EXIST %USERPROFILE%\AppData\Roaming\Waterfox\Profiles\p68xw6kw.default (
+			C:
+			cd %USERPROFILE%\AppData\Roaming\Waterfox\Profiles
+			
+			IF NOT EXIST %USERPROFILE%\AppData\Roaming\Waterfox\Profiles\p68xw6kw.default.backup (
+				echo "Unexpected Problem, no roaming backup folder found"
+				pause
+				exit
+			)
+			ren p68xw6kw.default.backup p68xw6kw.default
+		)
+		IF NOT EXIST %USERPROFILE%\AppData\Local\Waterfox\Profiles\p68xw6kw.default (
+			C:
+			cd %USERPROFILE%\AppData\Local\Waterfox\Profiles
+			
+			IF NOT EXIST %USERPROFILE%\AppData\Local\Waterfox\Profiles\p68xw6kw.default.backup (
+				echo "Unexpected Problem, no roaming backup folder found"
+				pause
+				exit
+			)
 			ren p68xw6kw.default.backup p68xw6kw.default
 		)
 	)
-)
 
-IF EXIST W:\ (
+	imdisk -a -s 512000k -m W: -p "/fs:ntfs /q /y"
+	:: echo "Before continuing, please verify that you would like the previous previous 
+	:: profile backup to be deleted"
+
+	C:
+	cd %USERPROFILE%\AppData\Roaming\Waterfox\Profiles
+	
+	IF EXIST %USERPROFILE%\AppData\Roaming\Waterfox\Profiles\p68xw6kw.default.backup (
+		:: echo "DEBUG - Roaming .backup found"
+		:: pause
+		rmdir /S /Q p68xw6kw.default.backup.previous.previous
+		ren p68xw6kw.default.backup.previous p68xw6kw.default.backup.previous.previous
+		ren p68xw6kw.default.backup p68xw6kw.default.backup.previous
+	)
+
+	C:
+	cd %USERPROFILE%\AppData\Roaming\Waterfox\Profiles
+
+	ren p68xw6kw.default p68xw6kw.default.backup
+	
+	IF NOT EXIST %USERPROFILE%\AppData\Roaming\Waterfox\Profiles\p68xw6kw.default.backup (
+		echo "Unexpected Problem, no roaming backup folder found"
+		pause
+		exit
+	)
+	
+	xcopy /S /I /E /H p68xw6kw.default.backup W:\Roaming\p68xw6kw.default
+	mklink /D "p68xw6kw.default" "W:\Roaming\p68xw6kw.default"
+
+	cd %USERPROFILE%\AppData\Local\Waterfox\Profiles
+
+	IF EXIST %USERPROFILE%\AppData\Local\Waterfox\Profiles\p68xw6kw.default.backup (
+		rmdir /S /Q p68xw6kw.default.backup.previous.previous
+		ren p68xw6kw.default.backup.previous p68xw6kw.default.backup.previous.previous
+		ren p68xw6kw.default.backup p68xw6kw.default.backup.previous
+	)
+	
+	C:
+	cd %USERPROFILE%\AppData\Local\Waterfox\Profiles
+	
+	ren p68xw6kw.default p68xw6kw.default.backup
+	
+	IF NOT EXIST %USERPROFILE%\AppData\Local\Waterfox\Profiles\p68xw6kw.default.backup (
+		echo "Unexpected Problem, no roaming backup folder found"
+		pause
+		exit
+	)
+	
+	xcopy /S /I /E /H p68xw6kw.default.backup W:\Local\p68xw6kw.default
+	mklink /D "p68xw6kw.default" "W:\Local\p68xw6kw.default"
+	
+) ELSE (
 	IF NOT EXIST W:\Roaming\p68xw6kw.default\ (
-		IF "%crashtest%" == "p68xw6kw.default" (		
+		IF "%crashtest%" == "p68xw6kw.default" (
 			echo "Computer crashed, or was otherwise restarted without copying the ramdisk profile back to the SSD.  Restoring from last backup, some browsing history was likely lost. Press any key to continue."
 			pause
 			cd %USERPROFILE%\AppData\Roaming\Waterfox\Profiles
@@ -46,62 +145,22 @@ IF EXIST W:\ (
 	IF EXIST W:\Roaming\p68xw6kw.default\ (
         echo "Skipping Ramdisk Creation, and Backup. Enter Y when prompted to backup your profile to the SSD from the Ramdisk."
     )
-) ELSE (
-    imdisk -a -s 512000k -m W: -p "/fs:ntfs /q /y"
-	echo "Before continuing, please verify that you would like the previous previous profile backup to be deleted"
-	pause
-	rmdir /S /Q p68xw6kw.default.backup.previous.previous 
-	IF EXIST %USERPROFILE%\AppData\Roaming\Waterfox\Profiles\p68xw6kw.default.backup (
-		ren p68xw6kw.default.backup.previous p68xw6kw.default.backup.previous.previous
-		ren p68xw6kw.default.backup p68xw6kw.default.backup.previous
-	)
-	cd %USERPROFILE%\AppData\Roaming\Waterfox\Profiles
-	C:
-	FOR /F "tokens=*" %%g IN ('dir /AL /B') DO (set crashtest=%%g)
-	IF NOT "%crashtest%" == "p68xw6kw.default" (
-		ren p68xw6kw.default p68xw6kw.default.backup
-	) ELSE (
-		echo "Symlink not expected!!!!"
-		pause
-		exit
-	)
-	xcopy /S /I /E /H p68xw6kw.default.backup W:\Roaming\p68xw6kw.default
-	mklink /D "p68xw6kw.default" "W:\Roaming\p68xw6kw.default"
-
-	cd %USERPROFILE%\AppData\Local\Waterfox\Profiles
-	rmdir /S /Q p68xw6kw.default.backup.previous.previous
-	IF EXIST %USERPROFILE%\AppData\Local\Waterfox\Profiles\p68xw6kw.default.backup (
-		ren p68xw6kw.default.backup.previous p68xw6kw.default.backup.previous.previous
-		ren p68xw6kw.default.backup p68xw6kw.default.backup.previous
-	)
-	cd %USERPROFILE%\AppData\Local\Waterfox\Profiles
-	C:
-	FOR /F "tokens=*" %%g IN ('dir /AL /B') DO (set crashtest=%%g)
-	IF NOT "%crashtest%" == "p68xw6kw.default" (
-		ren p68xw6kw.default p68xw6kw.default.backup
-	) ELSE (
-		echo "Symlink not expected!!!!"
-		pause
-		exit
-	)
-	xcopy /S /I /E /H p68xw6kw.default.backup W:\Local\p68xw6kw.default
-	mklink /D "p68xw6kw.default" "W:\Local\p68xw6kw.default"
-)
+) 
 
 :: If you want netflix to work use the line below, and comment out the other waterfox.exe line.
-::"C:\Program Files\Waterfox\waterfox.exe" "--enable-eme=widevine"
-
+:: "C:\Program Files\Waterfox\waterfox.exe" ::"--enable-eme=widevine"
 "C:\Program Files\Waterfox\waterfox.exe"
 :: Below command did absolutely nothing, there doesn't seem to be a way to disable this when executing from the command line this way.  I am manually disabling updater.exe
 :: -P "No Updates"
 
-cd %USERPROFILE%\AppData\Roaming\Waterfox\Profiles
 C:
-FOR /F "tokens=*" %%g IN ('dir /AL /B') DO (set crashtest=%%g)
+cd %USERPROFILE%\AppData\Roaming\Waterfox\Profiles
+
+FOR /F "tokens=*" %%g IN ('dir /AL /B 2^> nul') DO (set crashtest4=%%g)
 
 set /p deleteflag="Delete temporary ramdrive and restore profile?  Enter Y to delete, any other key to keep ramdrive and don't write back to the SSD (Don't Restart!)"
 if "%deleteflag%" == "Y" (
-	IF "%crashtest%" == "p68xw6kw.default" (
+	IF "%crashtest4%" == "p68xw6kw.default" (
 		cd %USERPROFILE%\AppData\Roaming\Waterfox\Profiles
 		C:
 		rmdir p68xw6kw.default
@@ -120,3 +179,10 @@ if "%deleteflag%" == "Y" (
     echo "Ramdisk is still cached, profile is not saved to the SSD."
 	pause
 )
+
+set deleteflag=
+set crashtest=
+set crashtest1=
+set crashtest2=
+set crashtest3=
+set crashtest4=
